2c1cfd3ba89188c624f858eac46e47768305a14d
Emilia Kytkova <milka.brisova@gmail.com>
2021-08-04 00:12:44 +0200

    created new home page and navmenu

--- Stopa_woodwork/ClientApp/src/components/Home.vue
+++ Stopa_woodwork/ClientApp/src/components/Home.vue
@@ -1,52 +1,106 @@
 <template>
-    <div class="hello">
-        <h1>{{ msg }}</h1>
-        <img src="../assets/logo.png" />
-        <p>
-            Template for Vue JS 3.0 and .NET 5 applications developed by <b>Alexandre Malavasi.</b><br />
-            In order to get more content on Vue JS and .NET platform, please follow on my social media profiles:
-        </p>
+    <div class="projects">
+      <div class="project-item">
+        <a href="single-project.html">
+          <img src="../assets/img1.jpeg" />
+        </a>
+        <h2 class="title">
+          <a href="#">Emotions</a>
+        </h2>
+      </div>
+
+      <div class="project-item">
+        <a href="single-project.html">
+          <img src="../assets/img2.jpeg" />
+        </a>
+        <h2 class="title">
+          <a href="#">Mountain</a>
+        </h2>
+      </div>
+
+      <div class="project-item">
+        <a href="single-project.html">
+          <img src="../assets/img3.jpeg" />
+        </a>
+        <h2 class="title">
+          <a href="#">Interface</a>
+        </h2>
+      </div>
+
+      <div class="project-item">
+        <a href="single-project.html">
+          <img src="../assets/img4.jpeg" />
+        </a>
+        <h2 class="title">
+          <a href="#">Transportation</a>
+        </h2>
+      </div>
+
+      <div class="project-item">
+        <a href="single-project.html">
+          <img src="../assets/img5.jpeg" />
+        </a>
+        <h2 class="title">
+          <a href="#">Perspective</a>
+        </h2>
+      </div>
+
+      <div class="project-item">
+        <a href="single-project.html">
+          <img src="../assets/img6.jpeg" />
+        </a>
+        <h2 class="title">
+          <a href="#">Mixilicious</a>
+        </h2>
+      </div>
+
+      <div class="project-item">
+        <a href="single-project.html">
+          <img src="../assets/img7.jpeg" />
+        </a>
+        <h2 class="title">
+          <a href="#">Creative</a>
+        </h2>
+      </div>
+
+      <div class="project-item">
+        <a href="single-project.html">
+          <img src="../assets/img8.jpeg" />
+        </a>
+        <h2 class="title">
+          <a href="#">Stapler</a>
+        </h2>
+      </div>
+
+      <!-- Pagination -->
+      <div class="pagination-wrap">
         <ul>
-            <li>
-                <a href="https://medium.com/@alexandre.malavasi" target="_blank">Medium</a>
-            </li>
-            <li>
-                <a href="https://twitter.com/alemalavasi" target="_blank">Twitter</a>
-            </li>
-            <li>
-                <a href="https://www.youtube.com/channel/UC-KFGgYiot1eA8QFqIgLmqA" target="_blank">Youtube</a>
-            </li>
-            <li>
-                <a href="https://www.facebook.com/alexandre.malavasi.dev" target="_blank">Facebook</a>
-            </li>
+          <li>
+            <a class="prev page-numbers" href="#">
+              <i class="fa fa-long-arrow-left"></i>
+            </a>
+          </li>
+          <li>
+            <a class="page-numbers" href="#">1</a>
+          </li>
+          <li>
+            <span class="page-numbers current">2</span>
+          </li>
+          <li>
+            <a class="page-numbers" href="#">3</a>
+          </li>
+          <li>
+            <a class="page-numbers" href="#">4</a>
+          </li>
+          <li>
+            <a class="next page-numbers" href="#">
+              <i class="fa fa-long-arrow-right"></i>
+            </a>
+          </li>
         </ul>
-        <h3>Vue Documentation</h3>
-        <p>
-            For a guide and recipes on how to configure / customize this project,<br>
-            check out the
-            <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-        </p>
-        <h3>Installed CLI Plugins</h3>
-        <ul>
-            <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-            <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-        </ul>
-        <h3>Essential Links</h3>
-        <ul>
-            <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-            <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-            <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-            <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-            <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-        </ul>
-        <h3>Ecosystem</h3>
-        <ul>
-            <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-            <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-            <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-            <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-            <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-        </ul>
+
+      </div>
+      <!-- End Pagination -->
     </div>
 </template>
 
